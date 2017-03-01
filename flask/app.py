9597a131929154367f6da0781b8ea6b2ef42c1a3
TetsuyaKomota <komota@ip-172-31-25-208.ap-northeast-1.compute.internal>
2017-03-01 06:05:48 +0000

    最初のコミット．Flask 入れた

---
+++ flask/app.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+
+from subprocess import check_output
+from flask import Flask, redirect, request, render_template
+from utils.decorator_auth import requires_auth
+
+from setting import HOSTNAME, PORT
+
+app = Flask(__name__)
+
+@app.before_request
+#@requires_auth
+def basic_auth():
+    pass
+
+@app.route("/")
+def index():
+    return render_template("sample.html")
+    # return agent.talk("hogehoge")
+
+
+if __name__ == "__main__":
+    app.run( debug = True, host=HOSTNAME, port=PORT)
