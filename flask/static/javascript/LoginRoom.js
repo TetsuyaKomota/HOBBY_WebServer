121d7370f7dbda5018af0899ec056a5eb8adae20
TetsuyaKomota <komota@ip-172-31-25-208.ap-northeast-1.compute.internal>
2017-04-28 10:49:01 +0000

    ログイン画面のコンポーネントだけ配置

--- flask/static/javascript/LoginRoom.js
+++ flask/static/javascript/LoginRoom.js
@@ -12,12 +12,58 @@ var TalkRoom = React.createClass(
         componentWillUnmount : function(){
         },
 
+        handleInput : function(){
+            console.log("inputed.");
+        },
+
         render : function(){
             return (
                 <div>
                     <h1>ひかりちゃんの部屋</h1>
                     <h2>エントランス</h2>
-                </div>
+                    <div>
+                        <p>ログイン</p>
+                        <form action="/" method="post">
+                            <p>
+                                ユーザーID:  <input type="text" name="client_id" value="0000000000" onChange={this.handleInput} size="100" />
+                            </p>
+                            <p>
+                                パスワード:    <input type="text" name="talk_id" onChange={this.handleInput} size="100" />
+                            </p>
+                            <p>
+                                <input type="submit" value="OK" />
+                            </p>
+                        </form>
+                    </div>
+                    <div>
+                        <p>新規登録</p>
+                        <form action="/" method="post">
+                            <p>
+                                ユーザーID:  <input type="text" name="client_id" value="0000000000" onChange={this.handleInput} size="100" />
+                            </p>
+                            <p>
+                                パスワード:    <input type="text" name="talk_id" onChange={this.handleInput} size="100" />
+                            </p>
+                            <p>
+                                <input type="submit" value="OK" />
+                            </p>
+                        </form>
+                    </div>
+                    <div>
+                        <p>退会</p>
+                        <form action="/" method="post">
+                            <p>
+                                ユーザーID:  <input type="text" name="client_id" value="0000000000" onChange={this.handleInput} size="100" />
+                            </p>
+                            <p>
+                                パスワード:    <input type="text" name="talk_id" onChange={this.handleInput} size="100" />
+                            </p>
+                            <p>
+                                <input type="submit" value="OK" />
+                            </p>
+                        </form>
+                    </div>
+               </div>
             );
         }
     }
