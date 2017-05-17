8ba8171c716d08c93dcdeef5bba1473f86580eb5
TetsuyaKomota <komota@ip-172-31-25-208.ap-northeast-1.compute.internal>
2017-05-17 07:24:38 +0000

    ログイン，新規登録を form から button + onClick に変更．ログイン成功時のリダイレクトを controller/HikariLogin から JS に変更．

--- flask/controllers/HikariLogin.py
+++ flask/controllers/HikariLogin.py
@@ -13,14 +13,14 @@ def hikariLogin(form, hikariMain):
     status = hikariMain.login(form["user_name"], form["password"])
 
     # ログイン失敗ならその旨を JSON で返して終了
+    output = {}
+    output['result'] = status[0]
+    output["message"] = status[1]
     if status[0] == False:
-        output = {}
-        output['result'] = status[0]
-        output["message"] = status[1]
         return json.dumps(output, indent=4, ensure_ascii=False)
     else:
         # Cookie を作成する
-        content = redirect("/talk_room")
+        content = json.dumps(output, indent=4, ensure_ascii=False)
         response = make_response(content) 
         max_age = HIKARI_SESSION_TIME
         expires = int(time.mktime(datetime.now().timetuple())) + max_age
--- flask/static/javascript/LoginRoom.js
+++ flask/static/javascript/LoginRoom.js
@@ -2,10 +2,11 @@ var TalkRoom = React.createClass(
     {
         getInitialState : function(){
             return {
-                talk_id     : "",
-                last_input  : 9999999999999,
-                delay       : 1000,
-                cu_message     : ""
+                talk_id       : "",
+                last_input    : 9999999999999,
+                delay         : 1000,
+                login_message : "",
+                cu_message    : ""
             };
         },
 
@@ -15,12 +16,30 @@ var TalkRoom = React.createClass(
         componentWillUnmount : function(){
         },
 
+        handleLogin : function(e){
+            var hashedPass = this.laundering(this.refs.login_user_name.value, this.refs.login_password.value);
+            $.ajax({
+                url: 'http://ec2-13-113-169-250.ap-northeast-1.compute.amazonaws.com:5000/api/hikari_login',
+                type: 'POST',
+                scriptCharset: 'UTF-8',
+                data: {
+                    'user_name' : this.refs.login_user_name.value,
+                    'password'  : hashedPass
+                },
+                dataType: 'json', 
+                cache: false,
+            }).done(function(data){
+                    this.setState({login_message:data.message});
+                    if(data.result == true){
+                        window.location.href = "/talk_room";
+                    }
+                }.bind(this))
+        },
+
+
         handleCreateUser : function(e){
             this.refs.cu_submit.disabled = true;
-            var hashedPass = this.hashing(this.refs.cu_password);
-            for(var i=0;i<1000;i++){
-                hashedPass = this.laundering(hashedPass, this.refs.cu_user_name, this.refs.cu_password);
-            }
+            var hashedPass = this.laundering(this.refs.cu_user_name.value, this.refs.cu_password.value);
             $.ajax({
                 url: 'http://ec2-13-113-169-250.ap-northeast-1.compute.amazonaws.com:5000/api/hikari_create_user',
                 type: 'POST',
@@ -49,13 +68,6 @@ var TalkRoom = React.createClass(
 
         checkValidation : function(target){
             this.refs.cu_submit.disabled = true;
-            var hashed = this.hashing(0);
-            for(var i=0;i<10;i++){
-                hashed = this.laundering(hashed, "user", 0);
-            }
-            // console.log(this.hashing("hogehoge"));
-            console.log("hashed : " + hashed);
-
             var date = new Date()
             if(date.getTime() - this.state.last_input < this.state.delay){
                 return;
@@ -85,18 +97,16 @@ var TalkRoom = React.createClass(
             return shaObj.getHash("HEX");
         },
 
-        laundering : function(hashedpass, user_id, password){
-            return this.hashing(hashedpass + this.hashing(user_id) + this.hashing(this.hashing(user_id) + password));
+        laundering : function(user_id, password){
+            var hashedpass = this.hashing(password);
+            for(var i=0;i<1000;i++){
+                hashedpass = this.hashing(hashedpass + this.hashing(user_id) + this.hashing(this.hashing(user_id) + password));
+            }
+            return hashedpass;
         },
 
         render : function(){
-            return (
-                <div>
-                    <h1>ひかりちゃんの部屋</h1>
-                    <h2>エントランス</h2>
-                    <div>
-                        <p>ログイン</p>
-                        <form action="/api/hikari_login" method="POST">
+/*                        <form action="/api/hikari_login" method="POST">
                             <p>
                                 ユーザーID:  <input type="text" name="user_name" size="100" />
                             </p>
@@ -107,6 +117,23 @@ var TalkRoom = React.createClass(
                                 <input type="submit" value="OK" />
                             </p>
                         </form>
+*/ 
+            return (
+                <div>
+                    <h1>ひかりちゃんの部屋</h1>
+                    <h2>エントランス</h2>
+                    <div>
+                        <p>ログイン</p>
+                            <p>
+                                ユーザーID:  <input ref="login_user_name" type="text" size="100" />
+                                {this.state.login_message}
+                            </p>
+                            <p>
+                                パスワード:    <input ref="login_password" type="password" size="100" />
+                            </p>
+                            <p>
+                                <input type="button" ref="login_submit" value="ログイン" onClick={this.handleLogin}/>
+                            </p>
                     </div>
                     <div>
                         <p>新規登録</p>
