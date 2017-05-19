cf36b6986bdfa58b2f1a3644c1ce4756604fdb05
TetsuyaKomota <komota@ip-172-31-25-208.ap-northeast-1.compute.internal>
2017-05-19 12:35:57 +0000

    自動ツイートを時報に変更

--- cron_batch/random_tweet.py
+++ cron_batch/random_tweet.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
+from datetime import datetime
 from requests_oauthlib import OAuth1Session
 
 from setting import HT_API_KEY
@@ -15,7 +16,22 @@ AS = HT_ACCESS_SECRET   # Accesss Token Secert
 url = "https://api.twitter.com/1.1/statuses/update.json"
 
 # ツイート本文
-params = {"status": "Hello, World!"}
+now = datetime.now()
+if ((now.hour + 9) % 24) < 4 or ((now.hour + 9) % 24) > 18:
+    reference = "こんばんは. "
+elif ((now.hour + 9) % 24) < 11:
+    reference = "おはよー. "
+else:
+    reference = "こんにちは. "
+
+reference = reference + "今は "
+reference = reference + str(now.month) + "月 "
+reference = reference + str(now.day) + "日の "
+reference = reference + str((now.hour + 9) % 24) + "時 "
+reference = reference + str(now.minute) + "分 "
+reference = reference + "だよ"
+
+params = {"status": reference}
 
 # OAuth認証で POST method で投稿
 twitter = OAuth1Session(CK, CS, AT, AS)
@@ -26,3 +42,15 @@ if req.status_code == 200:
     print ("OK")
 else:
     print ("Error: %d" % req.status_code)
+    print (req.json())
+
+# 現在日時をお知らせする
+def echo_currentTime(inputs):
+    talk_log, state, query = inputs
+
+    # 表情は normal に固定
+    # TODO いたるところに state のリテラルをぶち込んでる状態やめたい
+
+    # EC2 上ではなぜか9時間 遅れているようなので，調整
+    return ["normal", reference.decode("utf-8")]
+#
