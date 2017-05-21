aff1e1e8fad011582142fc7a991ffbf8bef8a3ae
TetsuyaKomota <komota@ip-172-31-25-208.ap-northeast-1.compute.internal>
2017-05-21 10:53:17 +0000

    ツイートに URL をくっつけた

--- cron_batch/random_tweet.py
+++ cron_batch/random_tweet.py
@@ -3,7 +3,10 @@ from datetime import datetime
 from datetime import timedelta
 from requests_oauthlib import OAuth1Session
 from random import random 
+import re
 
+from setting import HOSTNAME
+from setting import PORT
 from setting import HT_API_KEY
 from setting import HT_API_SECRET
 from setting import HT_ACCESS_KEY
@@ -27,23 +30,27 @@ url = "https://api.twitter.com/1.1/statuses/update.json"
 now = datetime.now() + timedelta(hours=9)
 if now.hour < 4 or now.hour > 18:
 # if ((now.hour + 9) % 24) < 4 or ((now.hour + 9) % 24) > 18:
-    reference = "こんばんは. "
+    response = "こんばんは. "
 elif now.hour < 11:
 # elif ((now.hour + 9) % 24) < 11:
-    reference = "おはよー. "
+    response = "おはよー. "
 else:
-    reference = "こんにちは. "
+    response = "こんにちは. "
 
-reference = reference + "今は "
-reference = reference + str(now.month) + "月 "
-reference = reference + str(now.day) + "日の "
-#reference = reference + str((now.hour + 9) / 24 + now.day) + "日の "
-reference = reference + str(now.hour) + "時 "
-#reference = reference + str((now.hour + 9) % 24) + "時 "
-reference = reference + str(now.minute) + "分 "
-reference = reference + "だよ"
+response = response + "今は "
+response = response + str(now.month) + "月 "
+response = response + str(now.day) + "日の "
+#response = response + str((now.hour + 9) / 24 + now.day) + "日の "
+response = response + str(now.hour) + "時 "
+#response = response + str((now.hour + 9) % 24) + "時 "
+response = response + str(now.minute) + "分 "
+response = response + "だよ"
 
-params = {"status": reference}
+talkRoomURL = "http://" + HOSTNAME + ":" + str(PORT) + "/talk_room"
+
+response = response + "\n" + talkRoomURL
+
+params = {"status": response}
 
 # OAuth認証で POST method で投稿
 twitter = OAuth1Session(CK, CS, AT, AS)
@@ -54,7 +61,7 @@ if req.status_code == 200:
     print ("OK")
 else:
     print ("Error: %d" % req.status_code)
-    print (req.json())
+    print (req.keys())
 
 # 現在日時をお知らせする
 def echo_currentTime(inputs):
@@ -64,5 +71,5 @@ def echo_currentTime(inputs):
     # TODO いたるところに state のリテラルをぶち込んでる状態やめたい
 
     # EC2 上ではなぜか9時間 遅れているようなので，調整
-    return ["normal", reference.decode("utf-8")]
+    return ["normal", response.decode("utf-8")]
 #
